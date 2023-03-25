95218294d25451c65f7cfb439e4ec1e7327929de
Николай Безруков <2sinsincuba@gmail.com>
2023-03-25 19:26:15 +0500

    Перенос проекта в новую ветку

---
+++ FP/link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/absracts/AbstractParser.java
@@ -0,0 +1,21 @@
+package ru.tinkoff.edu.java.linkparser.absracts;
+
+public abstract class AbstractParser {
+
+    private AbstractParser nextParser;
+
+    public void setNextParser(AbstractParser nextParser) {
+        this.nextParser = nextParser;
+    }
+
+    public String logParser (String link) {
+        if(nextParser != null) {
+            if(this.parsAbstract(link) == null) return nextParser.logParser(link);
+            return this.parsAbstract(link);
+        }
+        return null;
+    }
+
+    abstract protected String parsAbstract(String link);
+
+}
