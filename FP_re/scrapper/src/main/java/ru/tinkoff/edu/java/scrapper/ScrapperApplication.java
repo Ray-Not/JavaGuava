a80dd02015f57b68b30768caaf3391e84032127b
Николай Безруков <2sinsincuba@gmail.com>
2023-04-02 16:03:18 +0500

    Исправление планировщика

--- FP_re/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
+++ FP_re/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
@@ -3,6 +3,8 @@ package ru.tinkoff.edu.java.scrapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import ru.tinkoff.edu.java.linkparser.LinkParser;
 import ru.tinkoff.edu.java.scrapper.configuration.ApplicationConfig;
 
 @SpringBootApplication
@@ -11,5 +13,7 @@ public class ScrapperApplication {
 public static void main(String[] args) {
         var ctx = SpringApplication.run(ScrapperApplication.class, args);
         ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
+        LinkParser pars = new LinkParser();
+        System.out.println(pars.getLink("https://github.com/sanyarnd/tinkoff-java-course-2022/"));
         }
 }
--- FP_re/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/schedule/LinkUpdaterScheduler.java
+++ FP_re/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/schedule/LinkUpdaterScheduler.java
@@ -1,17 +1,17 @@
 package ru.tinkoff.edu.java.scrapper.schedule;
 
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-@EnableScheduling
+@Service
 public class LinkUpdaterScheduler {
     private final static Logger LOGGER =  Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     @Scheduled(fixedDelayString = "${app.scheduler.interval}")
     public void update() {
-        LOGGER.log(Level.INFO, "MSG INFO");
+        LOGGER.log(Level.INFO, "Info scrapper called");
     }
 }
