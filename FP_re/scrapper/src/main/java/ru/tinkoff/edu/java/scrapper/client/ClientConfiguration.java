76f1a45c158ba2e8e74badeb9ca7d0d770d4d7e8
Николай Безруков <2sinsincuba@gmail.com>
2023-04-03 21:20:33 +0500

    Клиент с prop

--- FP_re/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/client/ClientConfiguration.java
+++ FP_re/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/client/ClientConfiguration.java
@@ -1,15 +1,23 @@
 package ru.tinkoff.edu.java.scrapper.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 import ru.tinkoff.edu.java.linkparser.LinkParser;
 
+import java.util.Objects;
+
 
 @Configuration
 public class ClientConfiguration {
 
-    private final String BASE_URL = "https://jsonplaceholder.typicode.com";
+    private final String BASE_GIT_URL = "https://github.com/Ray-Not/JavaGuava";
+    private final String BASE_STACK_URL = "https://stackoverflow.com/questions/1642028/what-is-the-operator-in-c";
+    @Value("${git.link}")
+    String gitLink;
+    @Value("${stack.link}")
+    String stackLink;
     static WebClient.Builder builder = WebClient.builder();
     LinkParser pars = new LinkParser();
 
@@ -26,7 +34,10 @@ public class ClientConfiguration {
 
     @Bean
     public GitHubRecord gitHubClient() {
-        String gitLink = pars.getLink("https://github.com/Ray-Not/JavaGuava");
+
+        if(Objects.equals(gitLink, "")) gitLink = BASE_GIT_URL;
+
+        gitLink = pars.getLink(gitLink);
         GitHubRecord gitHubResponse = builder.build()
                 .get()
                 .uri("https://api.github.com/repos/" + gitLink)
@@ -38,8 +49,12 @@ public class ClientConfiguration {
 
     @Bean
     public StackOverflowRecord stackOverflowClient() {
-        String stackLink = pars.getLink("https://stackoverflow.com/questions/1642028/what-is-the-operator-in-c");
+
+
+        if(Objects.equals(stackLink, "")) stackLink = BASE_STACK_URL;
+
         String params = "?order=desc&sort=activity&site=stackoverflow";
+        stackLink = pars.getLink(stackLink);
         StackOverflowRecord stackOverflowResponse = builder.build()
                 .get()
                 .uri("https://api.stackexchange.com/2.3/questions/" + stackLink + params)
