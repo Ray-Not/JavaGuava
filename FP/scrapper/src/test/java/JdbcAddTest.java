cc8a642c82d6c956802a9ac486e2a975802e1387
Ray-Not <2sinsincuba@gmail.com>
2023-05-19 19:39:52 +0500

    Удаление левого теста, проверка workflows

--- FP/scrapper/src/test/java/JdbcAddTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-class JdbcAddTest extends IntegrationEnvironment {
-    @Test
-    void create_test() {
-//        Postgre_container.
-    }
-}
